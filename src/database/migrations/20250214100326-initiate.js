7afa09f0f91e14d715e46e8198c3db81c585d0e0
julio.abdilla@gmail.com <julio.abdilla@gmail.com>
2025-02-14 17:23:12 +0700

    add initiate migrations and seeders

---
+++ src/database/migrations/20250214100326-initiate.js
@@ -0,0 +1,609 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // Create `articles` table
+    await queryInterface.createTable('articles', {
+      id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      uuid: {
+        type: Sequelize.CHAR(36),
+        allowNull: false,
+      },
+      slug: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      title: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      content: {
+        type: Sequelize.TEXT('long'),
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.TINYINT,
+        allowNull: false,
+        defaultValue: 1,
+      },
+      published_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      tags: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      created_by: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: true,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_by: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: true,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      deleted_by: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: true,
+      },
+    });
+
+    // Create `galleries` table
+    await queryInterface.createTable('galleries', {
+      id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      uuid: {
+        type: Sequelize.CHAR(36),
+        allowNull: false,
+      },
+      slug: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      title: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      created_by: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: true,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_by: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: true,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      deleted_by: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: true,
+      },
+    });
+
+    // Create `medias` table
+    await queryInterface.createTable('medias', {
+      id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      uuid: {
+        type: Sequelize.CHAR(36),
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      data: {
+        type: Sequelize.BLOB('long'),
+        allowNull: false,
+      },
+      mimetype: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      last_access_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+
+    // Create `permissions` table
+    await queryInterface.createTable('permissions', {
+      id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+    });
+
+    // Create `roles` table
+    await queryInterface.createTable('roles', {
+      id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+    });
+
+    // Create `users` table
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      uuid: {
+        type: Sequelize.CHAR(36),
+        allowNull: false,
+      },
+      role_id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING(100),
+        allowNull: true,
+      },
+      username: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+
+    // Create `gallery_items` table
+    await queryInterface.createTable('gallery_items', {
+      id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      uuid: {
+        type: Sequelize.CHAR(36),
+        allowNull: true,
+      },
+      gallery_id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: false,
+      },
+      media_id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: true,
+      },
+      image_url: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      image_binary: {
+        type: Sequelize.BLOB,
+        allowNull: true,
+      },
+      caption: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      created_by: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: true,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_by: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: true,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      deleted_by: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: true,
+      },
+    });
+
+    // Create `role_permissions` table
+    await queryInterface.createTable('role_permissions', {
+      role_id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: false,
+      },
+      permission_id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: false,
+      },
+    });
+
+    // Create `staffs` table
+    await queryInterface.createTable('staffs', {
+      id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      uuid: {
+        type: Sequelize.CHAR(36),
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      image_url: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      image_binary: {
+        type: Sequelize.BLOB,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      created_by: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: true,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_by: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: true,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      deleted_by: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: true,
+      },
+    });
+
+    // Create `user_permission_revokes` table
+    await queryInterface.createTable('user_permission_revokes', {
+      user_id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: false,
+      },
+      permission_id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: false,
+      },
+    });
+
+    // Create `user_permissions` table
+    await queryInterface.createTable('user_permissions', {
+      user_id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: false,
+      },
+      permission_id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: false,
+      },
+    });
+
+    // Add foreign key constraints
+    await queryInterface.addConstraint('galleries', {
+      fields: ['created_by'],
+      type: 'foreign key',
+      name: 'gallery_user_FK',
+      references: {
+        table: 'users',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    });
+
+    await queryInterface.addConstraint('galleries', {
+      fields: ['updated_by'],
+      type: 'foreign key',
+      name: 'gallery_user_FK_1',
+      references: {
+        table: 'users',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    });
+
+    await queryInterface.addConstraint('galleries', {
+      fields: ['deleted_by'],
+      type: 'foreign key',
+      name: 'gallery_user_FK_2',
+      references: {
+        table: 'users',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    });
+
+    await queryInterface.addConstraint('gallery_items', {
+      fields: ['gallery_id'],
+      type: 'foreign key',
+      name: 'gallery_items_gallery_FK',
+      references: {
+        table: 'galleries',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    });
+
+    await queryInterface.addConstraint('gallery_items', {
+      fields: ['media_id'],
+      type: 'foreign key',
+      name: 'gallery_items_medias_FK',
+      references: {
+        table: 'medias',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    });
+
+    await queryInterface.addConstraint('gallery_items', {
+      fields: ['created_by'],
+      type: 'foreign key',
+      name: 'gallery_items_user_FK',
+      references: {
+        table: 'users',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    });
+
+    await queryInterface.addConstraint('gallery_items', {
+      fields: ['updated_by'],
+      type: 'foreign key',
+      name: 'gallery_items_user_FK_1',
+      references: {
+        table: 'users',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    });
+
+    await queryInterface.addConstraint('gallery_items', {
+      fields: ['deleted_by'],
+      type: 'foreign key',
+      name: 'gallery_items_user_FK_2',
+      references: {
+        table: 'users',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    });
+
+    await queryInterface.addConstraint('role_permissions', {
+      fields: ['role_id'],
+      type: 'foreign key',
+      name: 'role_permissions_roles_FK',
+      references: {
+        table: 'roles',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    });
+
+    await queryInterface.addConstraint('role_permissions', {
+      fields: ['permission_id'],
+      type: 'foreign key',
+      name: 'role_permissions_permissions_FK',
+      references: {
+        table: 'permissions',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    });
+
+    await queryInterface.addConstraint('staffs', {
+      fields: ['created_by'],
+      type: 'foreign key',
+      name: 'staffs_users_FK',
+      references: {
+        table: 'users',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    });
+
+    await queryInterface.addConstraint('staffs', {
+      fields: ['updated_by'],
+      type: 'foreign key',
+      name: 'staffs_users_FK_1',
+      references: {
+        table: 'users',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    });
+
+    await queryInterface.addConstraint('staffs', {
+      fields: ['deleted_by'],
+      type: 'foreign key',
+      name: 'staffs_users_FK_2',
+      references: {
+        table: 'users',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    });
+
+    await queryInterface.addConstraint('user_permission_revokes', {
+      fields: ['user_id'],
+      type: 'foreign key',
+      name: 'user_permission_revokes_users_FK',
+      references: {
+        table: 'users',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    });
+
+    await queryInterface.addConstraint('user_permission_revokes', {
+      fields: ['permission_id'],
+      type: 'foreign key',
+      name: 'user_permission_revokes_permissions_FK',
+      references: {
+        table: 'permissions',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    });
+
+    await queryInterface.addConstraint('user_permissions', {
+      fields: ['user_id'],
+      type: 'foreign key',
+      name: 'user_roles_users_FK',
+      references: {
+        table: 'users',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    });
+
+    await queryInterface.addConstraint('user_permissions', {
+      fields: ['permission_id'],
+      type: 'foreign key',
+      name: 'user_permissions_permissions_FK',
+      references: {
+        table: 'permissions',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    });
+
+    await queryInterface.addConstraint('users', {
+      fields: ['role_id'],
+      type: 'foreign key',
+      name: 'users_roles_FK',
+      references: {
+        table: 'roles',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('user_permissions');
+    await queryInterface.dropTable('user_permission_revokes');
+    await queryInterface.dropTable('staffs');
+    await queryInterface.dropTable('role_permissions');
+    await queryInterface.dropTable('gallery_items');
+    await queryInterface.dropTable('galleries');
+    await queryInterface.dropTable('articles');
+    await queryInterface.dropTable('users');
+    await queryInterface.dropTable('roles');
+    await queryInterface.dropTable('permissions');
+    await queryInterface.dropTable('medias');
+  },
+};
---
+++ src/database/seeders/20250214102055-initiate.js
@@ -0,0 +1,57 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // Insert into `users` table
+    await queryInterface.bulkInsert('users', [
+      {
+        id: 1,
+        uuid: '6d2e44f0-551d-4886-954f-942a6c23754e',
+        role_id: 1,
+        name: 'admin',
+        username: 'julio.abdilla@gmail.com',
+        password: '6b2860a6da44b826d9cae5f351c4ada7',
+        created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updated_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+        deleted_at: null,
+      },
+    ]);
+
+    // Insert into `permissions` table
+    await queryInterface.bulkInsert('permissions', [
+      { id: 1, name: 'user.read' },
+      { id: 2, name: 'user.create' },
+      { id: 3, name: 'user.update' },
+      { id: 4, name: 'user.delete' },
+      { id: 5, name: 'staff.read' },
+      { id: 6, name: 'staff.create' },
+      { id: 7, name: 'staff.update' },
+      { id: 8, name: 'staff.delete' },
+      { id: 9, name: 'gallery.read' },
+      { id: 10, name: 'gallery.create' },
+      { id: 11, name: 'gallery.update' },
+      { id: 12, name: 'gallery.delete' },
+      { id: 13, name: 'article.read' },
+      { id: 14, name: 'article.create' },
+      { id: 15, name: 'article.update' },
+      { id: 16, name: 'article.delete' },
+      { id: 17, name: 'me.read' },
+      { id: 18, name: 'me.update' },
+      { id: 19, name: 'me.delete' },
+    ]);
+
+    // Insert into `roles` table
+    await queryInterface.bulkInsert('roles', [
+      { id: 1, name: 'superuser' },
+      { id: 2, name: 'admin' },
+      { id: 3, name: 'staff' },
+    ]);
+  },
+
+  down: async (queryInterface) => {
+    // Delete all seeded data
+    await queryInterface.bulkDelete('users', { id: 1 });
+    await queryInterface.bulkDelete('permissions', { id: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19] });
+    await queryInterface.bulkDelete('roles', { id: [1, 2, 3] });
+  },
+};
